ecce75a47bdd0ba3ef49fe744b51d776235cf717
Abhishek Ojha <raunakojha60@gmail.com>
2022-12-29 19:08:24 +0530

    second commit

---
+++ VEMS/src/main/java/com/helper/connectionProvider.java
@@ -0,0 +1,23 @@
+package com.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class connectionProvider {
+	private static Connection con;
+	
+	public static Connection getConnection() {
+		try {
+			 if(con==null) {
+				 Class.forName("com.mysql.cj.jdbc.Driver");
+				 con= DriverManager.getConnection("jdbc:mysql://localhost:3306/visitorentry\", \"root\", \"root");
+			 }
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+
+}
